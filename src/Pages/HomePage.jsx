7f1627ed99f36e37e3df2bd9588785b23a51564d
Nouman Khan <noumankhan16859@gmail.com>
2025-03-27 20:44:56 +0530

    added 'who we are' section to HomePage with new background image and animations

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,17 +1,21 @@
-import React from "react";
+import React, { useRef } from "react";
 import heroImage from "../Assets/img1.jpg";
-import { Box, Button, Typography } from "@mui/material";
+import { Box, Button, Typography, useMediaQuery } from "@mui/material";
 import MoreIcon from "@mui/icons-material/KeyboardDoubleArrowRight";
-import { motion } from "framer-motion";
-
+import { motion, useInView } from "framer-motion";
+import whoWeAreImg from "../Assets/img2.jpg";
 const HomePage = () => {
+  const isSmallScreen = useMediaQuery("(max-width:600px)");
+  const ref = useRef(null);
+  const marginValue = isSmallScreen ? "-50px" : "-100px";
+  const isInView = useInView(ref, { margin: marginValue, once: false });
   return (
     <Box className="main" sx={{ minHeight: "100vh" }}>
       <section>
         <Box
           sx={{
             minHeight: "100vh",
-            backgroundImage: `url(${heroImage})`,
+            backgroundImage: `linear-gradient(rgba(107, 107, 107, 0.6), rgba(0, 0, 0, 0.6)), url(${heroImage})`,
             backgroundSize: "cover",
             backgroundPosition: "center",
             backgroundRepeat: "no-repeat",
@@ -42,8 +46,8 @@ const HomePage = () => {
               zIndex: 3,
             }}
             component={motion.div}
-            initial={{ opacity: 0, y: 0 }} 
-            animate={{ opacity: 1, y: -130 }} 
+            initial={{ opacity: 0, y: 0 }}
+            animate={{ opacity: 1, y: -130 }}
             transition={{ duration: 0.8, ease: "easeInOut" }}
           >
             <Typography
@@ -237,6 +241,110 @@ const HomePage = () => {
               }}
               color="inherit"
             >
+              Read More <MoreIcon sx={{ ml: 1 }} />
+            </Button>
+          </Box>
+        </Box>
+      </section>
+      <section>
+        <Box
+          className="Who we are Main Div"
+          sx={{
+            backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)), url(${whoWeAreImg})`,
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            backgroundRepeat: "no-repeat",
+            position: "relative",
+            minHeight: "100vh",
+            display: "flex",
+            flexWrap: "wrap",
+          }}
+        >
+          <Box
+            ref={ref}
+            className="name div"
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              margin: "auto",
+              height: "fit-content",
+              width: { md: "fit-content", xs: "100%" },
+              ml: { md: "7rem", xs: "0" },
+              mt: { md: "5rem", xs: "1rem" },
+              textAlign: { md: "unset", xs: "center" },
+            }}
+            component={motion.div}
+            initial={
+              isSmallScreen ? { opacity: 0, y: 100 } : { opacity: 0, x: -70 }
+            }
+            animate={
+              isInView
+                ? { opacity: 1, y: 0, x: 0 }
+                : isSmallScreen
+                ? { opacity: 0, y: 100 }
+                : { opacity: 0, x: -70 }
+            }
+            transition={{ duration: 1, ease: "easeInOut" }}
+          >
+            <Typography
+              variant="h3"
+              sx={{
+                fontSize: { md: "4.5rem", xs: "3rem" },
+                color: "white",
+                fontWeight: "bold",
+              }}
+            >
+              Who
+            </Typography>
+            <Typography
+              variant="h3"
+              sx={{
+                fontSize: { md: "4.5rem", xs: "3rem" },
+                color: "white",
+                fontWeight: "bold",
+                pl: { md: 7 },
+              }}
+            >
+              We Are
+            </Typography>
+          </Box>
+          <Box
+          ref={ref}
+            className="Intro div"
+            sx={{
+              width: { md: "40%", xs: "100%" },
+              mt: { md: "7rem", xs: "0" },
+              mr: { md: "3rem", xs: "0" },
+              textAlign: { md: "unset", xs: "center" },
+              px: { xs: "1.5rem" },
+            }}
+            component={motion.div}
+            initial={
+              isSmallScreen ? { opacity: 0, y: 100 } : { opacity: 0, x: 70 }
+            }
+            animate={
+              isInView
+                ? { opacity: 1, y: 0, x: 0 }
+                : isSmallScreen
+                ? { opacity: 0, y: 100 }
+                : { opacity: 0, x: 70 }
+            }
+            transition={{ duration: 1, ease: "easeIn" }}
+          >
+            <Typography variant="body1" color="white">
+              Firmus Vision is one of the fastest growing PR and Communications,
+              Event Management and Advertising Agency in the industry that
+              provides one-stop solution for all type of media communication
+              services. We provide focused services for public and media
+              relations, corporate and crisis communications, image and
+              brandmanagement and issue management and content creation. Our
+              greatest strength lies in our robust media relations and
+              penetration, pro-active approach towards problems and content and
+              message development. We help you tell your stories in the media
+              positively and effectively in order to build and enhance your
+              brand and image.
+            </Typography>
+            <Button variant="text" sx={{ color: "orange", fontWeight: "bold" }}>
               Read More <MoreIcon sx={{ ml: 1 }} />
             </Button>
           </Box>
